25c5b6db2ee30c6c9d39d6b7c8cbf4b0a6a557d0
Shane Osbourne <shane.osbourne8@gmail.com>
2014-11-12 08:38:50 +0000

    Word on passing args around

--- setup.js
+++ setup.js
@@ -1,47 +1,42 @@
 var async       = require("async");
-
-var logger      = require("eazy-logger").Logger({
-    prefix: "{magenta:[WDIFU] ",
-    useLevelPrefixes: true,
-    custom: {
-        "i": function (string) {
-            return this.compile("{cyan:" + string + "}");
-        }
-    }
-});
-
-var run         = require("./run")(logger);
-
-var tests       = [
-    {
-        hash: "3b358a56e2ede7927cf"
-    },
-    {
-        hash: "e1e418dfa7c7e9640bce6779310e795eefbe1ca3"
-    }
-];
+//var compare     = require("compare");
+var logger      = require("./lib/logger").logger;
+var run         = require("./lib/_run")(logger);
 
 var config = {
     base: "http://selco.static:8000",
-    cwd: "/Users/shakyshane/code/selco-static"
+    outDir: "screenshots",
+    cwd: "/Users/shakyshane/code/selco-static",
+    tests: [
+        {
+            hash: "3b358a56e2ede7927cf"
+        },
+        {
+            hash: "e1e418dfa7c7e9640b"
+        }
+    ],
+    paths: [
+        "/buy/homepage.php"
+    ]
 }
 
-async.eachSeries(tests, function (item, cb) {
+logger.info("Working in: {yellow:%s", config.cwd);
+config.paths.forEach(function (path) {
+    logger.debug("Adding {yellow:%s} to the queue", config.cwd);
+});
 
-    logger.info("Working in: {yellow:%s", config.cwd);
-
-    logger.info("Running against: {yellow:%s", item.hash.slice(0, 5));
-
-    process.env["WDIFU_HASH"] = item.hash;
-    process.env["WDIFU_BASE"] = config.base;
+async.eachSeries(config.tests, function (item, cb) {
+    process.env["WDIFU_HASH"]    = item.hash;
+    process.env["WDIFU_BASE"]    = config.base;
+    process.env["WDIFU_PATHS"]   = config.paths.join(",");
+    process.env["WDIFU_OUT_DIR"] = config.outDir;
 
     run(item, config, function (err, out) {
-
-        if (out) {
-            console.log(out);
-        }
         if (err) {
             return cb(err);
+            if (out) {
+                console.log(out);
+            }
         }
         cb();
     });
--- test/e2e.home.js
+++ test/e2e.home.js
@@ -11,6 +11,11 @@ function writeScreenShot(data, dir, filename) {
     var stream = fs.createWriteStream(path.join(dir, filename));
     stream.write(new Buffer(data, 'base64'));
     stream.end();
+    if (!process.env["WDIFU_OUT"]) {
+        process.env["WDIFU_OUT"] = filename;
+    }  else {
+        process.env["WDIFU_OUT"] += filename;
+    }
 }
 
 function createScreenshotName(filepath) {
@@ -22,65 +27,78 @@ function getScreens (height, increment) {
     console.log(Math.ceil(height / increment));
 }
 
-var urls = [
-    "/buy/homepage.php"
-];
-
 describe('Section Navigation', function() {
     var ptor = protractor.getInstance();
-    var height;
-    var inc;
-    var hash;
+    var height, increment, hash, paths, outDir;
     beforeEach(function () {
-        inc = 480;
-        hash = process.env["WDIFU_HASH"];
+        increment = 480;
+        hash    = process.env["WDIFU_HASH"];
+        outDir  = process.env["WDIFU_OUT_DIR"];
+        paths   = process.env["WDIFU_PATHS"].split(",");
         browser.ignoreSynchronization = true;
-        browser.manage().window().setSize(500, 588);
+        browser.manage().window().setSize(320, 588);
     });
     it("should contain the BS script element", function () {
-        browser.get(urls[0]);
-        doone(inc, hash, "homepage");
-        //browser.get(urls[1]);
-        //doone(inc, "654321", "product-1");
-        //browser.get(urls[2]);
-        //doone(inc, "654321", "product-2");
+        var flow = protractor.promise.controlFlow();
+        paths.forEach(function (url) {
+            browser.get(url);
+            screengrabOnePage({
+                increment: increment,
+                hash: hash,
+                url: url,
+                outDir: outDir
+            });
+        });
+        flow.execute(function () {
+            process.env["SHANE"] = "awes";
+        })
     });
 });
 
-function doone(inc, dir, name) {
+function getFilename (url, i) {
+    return path.basename(url).replace(path.extname(url), "") + "-" + i + ".png";
+}
+
+function screengrabOnePage(opts, state) {
 
     var ptor = protractor.getInstance();
+    var flow = protractor.promise.controlFlow();
 
     // Get document height from browser
-    ptor.executeScript('return document.body.clientHeight').then(function (out) {
+    return ptor.executeScript('return document.body.clientHeight').then(function (out) {
 
         // Get amount of screens needed
-        var count = Math.ceil(out/inc);
+        var count = Math.ceil(out/opts.increment);
         var tasks = [];
 
         for (var i = 0, n = count; i < n; i += 1) {
-            tasks.push(i * inc);
+            tasks.push(i * opts.increment);
         }
-
-        var flow = protractor.promise.controlFlow();
 
         // push tasks onto the web driver stack
         tasks.forEach(function (val, i) {
             flow.execute(function () {
-                return doScreenGrab(i, val, dir, name);
+                getScreenFileInfo(i, val, opts);
+                return doScreenGrab(i, val, getScreenFileInfo(i, val, opts));
             });
         });
     });
 }
 
+function getScreenFileInfo (i, val, opts) {
+    return {
+        dir:      path.join(opts.outDir, opts.hash.slice(0, 8)),
+        filename: getFilename(opts.url, i+1)
+    }
+}
 
-function doScreenGrab (i, inc, dir, name) {
+
+function doScreenGrab (i, inc, fileOpts) {
     var ptor = protractor.getInstance();
     return ptor.executeScript('window.scrollTo(0,'+(inc)+');').then(function (out) {
         ptor.executeScript('return window.scrollY').then(function (out) {});
         return browser.takeScreenshot();
     }).then(function (png) {
-        var filename = name + "-" + i + ".png";
-        return writeScreenShot(png, "screenshots/" + dir.slice(0, 5), filename);
+        return writeScreenShot(png, fileOpts.dir, fileOpts.filename);
     });
 }
